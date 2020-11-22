588abe9cb71441e51c306f435278134bd7238031
Ellie Brayton <ebrayton@ufl.edu>
2020-11-21 19:30:39 -0600

    Edited modal

--- js/main.js
+++ js/main.js
@@ -29,3 +29,31 @@ for (i = 0; i < accordion.length; i++) {
     } 
   });
 }
+
+//MODAL
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+
+window.addEventListener("load",
+  function() {
+    setTimeout(showPopup, 1500);
+  });
+
+function showPopup() {
+  document.getElementById("myModal").style.opacity = 1;
+}
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
