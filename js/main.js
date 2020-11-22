9cd0c0a8523c83d8f887f289663c857ea67b3d8b
Ellie Brayton <ebrayton@ufl.edu>
2020-11-21 18:40:24 -0600

    Added modal structure to HTML

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+//HAMBURGER MENU 
+
 var hamburger = document.getElementById('hamburger');
 var menu = document.getElementById('menu');
 
@@ -10,3 +12,20 @@ hamburger.addEventListener('click', function() {
     hamburger.classList.toggle('active');
     menu.classList.toggle('show');
   });
+
+//ACCORDION
+
+var accordion = document.getElementsByClassName("accordion");
+
+
+for (i = 0; i < accordion.length; i++) {
+  accordion[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
